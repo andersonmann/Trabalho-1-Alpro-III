e601bf45f38380ef24582a48968725077149ef81
anderson <anderson.civil@hotmail.com>
2016-05-15 00:08:36 -0300

    Criado classe App

---
+++ T1_alproIII_2016-1/src/pucrs/alproIII_2016_1/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pucrs.alproIII_2016_1;
+
+/**
+ * @author Anderson
+ *
+ */
+public class App{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
